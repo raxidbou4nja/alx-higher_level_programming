62005ef7732634109026dc05e1d31d378e9c7322
raxidbou4nja <rachid.boughnja@gmail.com>
2023-09-05 19:56:01 +0000

    fix task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
+        number = i * 10 + j
+        separator = "\n" if number == 89 else ", "
         if i != j:
-            print("{:02d}".format(i * 10 + j), end=""  if i * 10 + j  == 89 else ", ")
+            print("{:02d}".format(number), end=separator)
