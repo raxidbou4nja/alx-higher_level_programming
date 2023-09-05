a4556e6f106eca42f56fa3bf247a2a7f7cba2b13
raxidbou4nja <rachid.boughnja@gmail.com>
2023-09-05 23:48:57 +0000

    add new task file

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,4 @@ for ascii_value in range(ord('Z'), ord('A') - 1, -1):
     char_to_print = chr(ascii_value)
     if ascii_value % 2 == 0:
         char_to_print = char_to_print.lower()
-    print(char_to_print, end="")
+    print("{}".format(char_to_print), end="")
