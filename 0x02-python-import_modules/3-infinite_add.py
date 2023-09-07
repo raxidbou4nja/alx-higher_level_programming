caaef8524abc0e0a45fb1048dc4e8c6a184c901b
raxidbou4nja <rachid.boughnja@gmail.com>
2023-09-07 22:06:35 +0000

    fix task 3 file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,10 +3,11 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
         print("0")
-        return
+        sys.exit(0)
     
     try:
         result = sum(int(arg) for arg in sys.argv[1:])
         print("{}".format(result))
     except ValueError:
         print("Error: Some arguments are not integers.")
+        sys.exit(1)
