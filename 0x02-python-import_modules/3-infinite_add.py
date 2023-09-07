07a498fbb392d5b114063c941b7ff1f16c9c8617
raxidbou4nja <rachid.boughnja@gmail.com>
2023-09-07 22:07:34 +0000

    fix task 3 file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,6 @@ if __name__ == "__main__":
     if len(sys.argv) <= 1:
         print("0")
         sys.exit(0)
-    
     try:
         result = sum(int(arg) for arg in sys.argv[1:])
         print("{}".format(result))
