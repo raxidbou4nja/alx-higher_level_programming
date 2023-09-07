509f28d79d6b3c049dde73ab1d9cc5bfa77b5036
raxidbou4nja <rachid.boughnja@gmail.com>
2023-09-07 21:31:05 +0000

    fix task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         print("{} argument:".format(args - 1))
     else:
         print("{} arguments:".format(args - 1))
-    for x in range(1, args):
-        print("{}: {}".format(x, sys.argv[x]))
+    for index, argument in enumerate(sys.argv[1:], start=1):
+        print("{}: {}".format(index, argument))
