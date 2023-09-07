ae87c9869b95a17d6036337cbe1c2d36a42d2e54
raxidbou4nja <rachid.boughnja@gmail.com>
2023-09-07 22:11:01 +0000

    fix task 2 file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 import sys
+
 if __name__ == "__main__":
     args = len(sys.argv)
-    if(args <= 1):
+    if args <= 1:
         print("{} arguments.".format(args - 1))
-    elif(args == 2):
+    elif args == 2:
         print("{} argument:".format(args - 1))
     else:
         print("{} arguments:".format(args - 1))
+    
     for index, argument in enumerate(sys.argv[1:], start=1):
         print("{}: {}".format(index, argument))
