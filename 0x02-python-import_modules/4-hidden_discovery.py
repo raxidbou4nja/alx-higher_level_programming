2cf37adda8612735ea2e4ae8e12d295a0f926b64
raxidbou4nja <rachid.boughnja@gmail.com>
2023-09-07 22:01:05 +0000

    fix errors

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
+import hidden_4
+
+def main():
     for name in dir(hidden_4):
-        if name[0] != '_' and name[1] != '_':
+        if not name.startswith('__'):
             print(name)
+
+if __name__ == "__main__":
+    main()
