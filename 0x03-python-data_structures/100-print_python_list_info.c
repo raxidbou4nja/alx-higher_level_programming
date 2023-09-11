dbde64ada01e9d90b5352c0f85007d78b23dbb9a
raxidbou4nja <rachid.boughnja@gmail.com>
2023-09-11 23:16:07 +0000

    add task 14 file

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,13 @@
+#include "object.h"
+#include "listobject.h"
+void print_python_list_info(PyObject *p)
+{
+    int index
+    int l;
+
+    l = PyList_Size(p)
+    for (i = 0 ; i < l ; i++)
+    {
+            printf("[*] Size of the Python List = %d", l);
+    }
+}
