ca1295de600f6ecb770744fb0ca23dc624936e0d
raxidbou4nja <rachid.boughnja@gmail.com>
2023-09-11 23:58:19 +0000

    fix one error

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,13 +1,10 @@
-#include "object.h"
 #include "listobject.h"
+#include "object.h"
 void print_python_list_info(PyObject *p)
 {
-    int index;
-    int l;
+	int i, l;
 
-    l = PyList_Size(p)
-    for (i = 0 ; i < l ; i++)
-    {
-            printf("[*] Size of the Python List = %d", l);
-    }
+	l = PyList_Size(p)
+	for (i = 0 ; i < l ; i++)
+		printf("[*] Size of the Python List = %d", l);
 }
