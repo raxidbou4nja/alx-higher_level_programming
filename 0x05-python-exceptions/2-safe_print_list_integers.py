9f53d2070367caf93bc9ad2f42ed87a753b510d9
raxidbou4nja <rachid.boughnja@gmail.com>
2023-09-25 20:51:02 +0000

    add new file

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
                 if count == x:
                     break
-    except TypeError:
+    except (TypeError, ValueError):
         pass
 
     print()
