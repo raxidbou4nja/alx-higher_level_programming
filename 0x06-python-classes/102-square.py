b77bc8697f2ec46200543352ad08ac445fd6f71a
raxidbou4nja <125773922+raxidbou4nja@users.noreply.github.com>
2023-09-27 01:29:49 +0100

    Update 102-square.py

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,53 +1,49 @@
 #!/usr/bin/python3
-"""Coordinates of a square"""
-
-
 class Square:
-    """Private instance attribute: size
-    Instantiation with area and position method """
+    """Defines a square with size and comparison methods."""
 
     def __init__(self, size=0):
-        """Initializes attribute size """
+        """Initializes the square with a given size."""
         self.size = size
 
     def area(self):
-        """Calculate area of square"""
-        return (self.__size * self.__size)
+        """Calculates and returns the area of the square."""
+        return self.size ** 2
 
     @property
     def size(self):
-        """Getter for square"""
+        """Getter method for the size."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Initializes attribute size """
-        if (type(value) is not int):
+        """Setter method for the size."""
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def __eq__(self, other):
-        """Equal"""
+        """Equal comparison."""
         return self.size == other.size
 
     def __ne__(self, other):
-        """Not Equal"""
+        """Not equal comparison."""
         return self.size != other.size
 
     def __lt__(self, other):
-        """Less than"""
+        """Less than comparison."""
         return self.size < other.size
 
     def __le__(self, other):
-        """Less than or equal"""
+        """Less than or equal comparison."""
         return self.size <= other.size
 
     def __gt__(self, other):
-        """Greater than"""
+        """Greater than comparison."""
         return self.size > other.size
 
     def __ge__(self, other):
-        """Greater than or equal"""
+        """Greater than or equal comparison."""
         return self.size >= other.size
