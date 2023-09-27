e782e724dbfba632c22607c7164b897d08fe82c7
raxidbou4nja <125773922+raxidbou4nja@users.noreply.github.com>
2023-09-27 01:05:10 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 class Square:
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
@@ -11,7 +12,7 @@ class Square:
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
@@ -21,7 +22,11 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(val, int) and val >= 0 for val in value):
+        if (
+            not isinstance(value, tuple)
+            or len(value) != 2
+            or not all(isinstance(num, int) and num >= 0 for num in value)
+        ):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
