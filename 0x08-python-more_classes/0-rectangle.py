d4d7f051535d5fbacb50b5f3333872644211ebcb
raxidbou4nja <rachid.boughnja@gmail.com>
2023-10-02 18:42:14 +0000

    add task 0 file

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-This is the "Rectangle" module.
-
-This module provides a simple Rectangle class.
+This is the "Rectangle" module
+This module defines a basic Rectangle class
 """
 
 
 class Rectangle:
-    """A simple Rectangle class"""
+    """
+    A simple class to represent a rectangle
+    """
     pass
