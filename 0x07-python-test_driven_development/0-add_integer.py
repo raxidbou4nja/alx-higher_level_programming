3b7bb42b93f7404878f61747245926404c2741ad
raxidbou4nja <rachid.boughnja@gmail.com>
2023-10-04 02:32:21 +0000

    add files

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,11 @@
+#!/usr/bin/python3
 def add_integer(a, b=98):
-    # Check if a and b are either integers or floats
-    if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
-        raise TypeError("Both a and b must be integers or floats")
-    
-    # Convert a and b to integers if they are floats
-    a = int(a) if isinstance(a, float) else a
-    b = int(b) if isinstance(b, float) else b
-    
-    # Perform the addition
-    result = a + b
-    
-    return result
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return (a + b)
