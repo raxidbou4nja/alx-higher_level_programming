86317331f70fc4b71d330236e67ab307e1031d77
raxidbou4nja <rachid.boughnja@gmail.com>
2023-10-09 23:44:08 +0000

    fix errors

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 """
-=============================
-Module with the method lookpu
-=============================
+lookup module
+
 """
 
 
 def lookup(obj):
     """Function for return the attributes for an object"""
 
-    att_and_methods = dir(obj)
-    filtered = [item for item in att_and_methods if not item.startswith('_')]
-    return (filtered)
+    return dir(obj)
