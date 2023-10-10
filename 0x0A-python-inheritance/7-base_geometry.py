605e48129d755f4fc99e579ad5a9e4a6806b8a7b
raxidbou4nja <rachid.boughnja@gmail.com>
2023-10-10 00:42:50 +0000

    fix chmod

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,39 +1,18 @@
 #!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 """
-A module with a class called BaseGeometry.
+===================================
+module with class Rectangle
+===================================
 """
 
 
-class BaseGeometry:
-    """
-    BaseGeometry class with methods for calculating
-    area and integer validation.
-    """
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherits from BaseGeometry"""
 
-    def area(self):
-        """
-        Abstract method for calculating the area.
-
-        Raises:
-            Exception: This method is not implemented in the base class.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validate if a number is an integer and greater than zero.
-
-        Args:
-            name (str): The name of the value being
-            validated (for error messages).
-            value: The value to be validated.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to zero.
-        """
-
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
