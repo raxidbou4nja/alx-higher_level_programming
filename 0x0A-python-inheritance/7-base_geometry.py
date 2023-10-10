39d14f985b940caac2b8975cb4a0c2d72d49fe14
raxidbou4nja <rachid.boughnja@gmail.com>
2023-10-10 00:25:06 +0000

    add new file

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -4,17 +4,36 @@ A module with a class called BaseGeometry.
 """
 
 
-from abc import ABC, abstractmethod
-
-
-class BaseGeometry(ABC):
+class BaseGeometry:
     """
-    BaseGeometry class with an abstract method for calculating area.
+    BaseGeometry class with methods for calculating
+    area and integer validation.
     """
 
-    @abstractmethod
     def area(self):
         """
         Abstract method for calculating the area.
+
+        Raises:
+            Exception: This method is not implemented in the base class.
         """
-        pass
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validate if a number is an integer and greater than zero.
+
+        Args:
+            name (str): The name of the value being
+            validated (for error messages).
+            value: The value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to zero.
+        """
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
