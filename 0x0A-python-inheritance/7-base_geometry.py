0bcb1ecef8ba2975d23916cf4d329482ff60d5b0
raxidbou4nja <rachid.boughnja@gmail.com>
2023-10-10 00:44:21 +0000

    fix chmod

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
 """
 ===================================
-module with class Rectangle
+module with class BaseGeometry
 ===================================
 """
 
 
-class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry"""
+class BaseGeometry:
+    """BaseGeometry class"""
 
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
+    def area(self):
+        """method for calculated area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Method for validate if a num is integer"""
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
