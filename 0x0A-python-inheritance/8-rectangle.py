04a05ea115dc7485347ced46c90769c20fb1ae93
raxidbou4nja <rachid.boughnja@gmail.com>
2023-10-10 00:30:28 +0000

    fix chmod

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,16 +2,24 @@
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 """
-===================================
-module with class Rectangle
-===================================
+A module with a class called Rectangle that inherits from BaseGeometry.
 """
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry"""
+    """
+    Rectangle class that inherits from BaseGeometry.
+    """
 
     def __init__(self, width, height):
+        """
+        Initialize a Rectangle object with width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        # Validate and set the width and height attributes
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
