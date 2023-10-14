448f73dc75b3e9037109745404a60c099f05e68f
raxidbou4nja <rachid.boughnja@gmail.com>
2023-10-14 18:49:00 +0000

    add files from 18 to 19

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -70,3 +70,41 @@ class Base:
         if json_string is None or json_string == "":
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Create an instance with attributes set from a dictionary.
+
+        Args:
+            cls: The class.
+            **dictionary: A dictionary with attribute values.
+
+        Returns:
+            cls: An instance of the class with attributes set from the dictionary.
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Load a list of instances from a JSON file.
+
+        Args:
+            cls: The class.
+
+        Returns:
+            list: List of instances based on the current class and data from the JSON file.
+        """
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, 'r') as file:
+                json_data = file.read()
+                dict_list = cls.from_json_string(json_data)
+                instance_list = [cls.create(**d) for d in dict_list]
+                return instance_list
+        except FileNotFoundError:
+            return []
