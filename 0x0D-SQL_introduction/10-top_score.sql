b563a6ad3dd782be1a692873337163dd8d0bd429
raxidbou4nja <rachid.boughnja@gmail.com>
2023-11-14 16:13:49 -0800

    add files

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- Lists all records and order by descending score.
+SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC;
---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- To List all records where score >= 10 and ordered by descending score.
+SELECT `score`, `name` FROM `second_table` WHERE `score` >= 10 ORDER BY `score` DESC;
