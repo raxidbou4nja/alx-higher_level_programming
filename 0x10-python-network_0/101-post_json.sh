b805c7140231208bc97eefbc2c0c711554a032a0
raxidbou4nja <rachid.boughnja@gmail.com>
2024-01-05 16:27:18 -0800

    update

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes a POST request to a specified URL with a JSON payload
-curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
+curl -sX POST -d "@$2" -H "Content-Type: application/json" "$1"
