5f3986aaf480d54e760491cd87a85674855332b8
raxidbou4nja <rachid.boughnja@gmail.com>
2024-01-07 15:56:06 -0800

    add new file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""Fetche response https://intranet.hbtn.io/status."""
+"""Fetch respone https://intranet.hbtn.io/status."""
 import urllib.request
 
-
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    request = urllib.request.Request("https://intranet.hbtn.io/status", headers=headers)
+
+    try:
+        with urllib.request.urlopen(request) as response:
+            body = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(body)))
+            print("\t- content: {}".format(body))
+            print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    except urllib.error.HTTPError as e:
+        print("HTTP Error {}: {}".format(e.code, e.reason))
