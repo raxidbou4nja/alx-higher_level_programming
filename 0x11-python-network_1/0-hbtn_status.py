bdcd33b31aac7f8260a88e458975f309820f4ea6
raxidbou4nja <rachid.boughnja@gmail.com>
2024-01-07 16:02:10 -0800

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""Fetch respone https://intranet.hbtn.io/status."""
+"""
+Fetch response https://intranet.hbtn.io/status
+"""
+
 import urllib.request
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
 
-if __name__ == "__main__":
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    request = urllib.request.Request("https://intranet.hbtn.io/status", headers=headers)
-
-    try:
-        with urllib.request.urlopen(request) as response:
-            body = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(body)))
-            print("\t- content: {}".format(body))
-            print("\t- utf8 content: {}".format(body.decode("utf-8")))
-    except urllib.error.HTTPError as e:
-        print("HTTP Error {}: {}".format(e.code, e.reason))
+print('Body response:\n\t- type: {}'.format(type(html)))
+print('\t- content: {}'.format(html))
+print('\t- utf8 content: {}'.format(html.decode('utf-8')))
