ab6a9c7726f2cae3743d43f26f304c9f224ea1f6
raxidbou4nja <rachid.boughnja@gmail.com>
2024-01-07 16:12:41 -0800

    add new file

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -19,5 +19,9 @@ if __name__ == '__main__':
             print("No result")
         else:
             print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
-    except:
-        print("Not a valid JSON")
+    except requests.exceptions.RequestException as e:
+        print("Request Error: {}".format(e))
+    except ValueError as e:
+        print("JSON Decode Error: {}".format(e))
+    except Exception as e:
+        print("Error: {}".format(e))
