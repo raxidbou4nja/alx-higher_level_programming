daf5b77912ac1b01f7b59de6c210c23b4c7a8224
raxidbou4nja <raxid.bou4nja@gmail.com>
2024-01-30 15:44:21 -0800

    add new files

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: node 0-readme.js <file path>');
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
