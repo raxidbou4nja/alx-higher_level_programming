88d79057b676e84c0d6e6acd7bc956e3b423f0f7
raxidbou4nja <raxid.bou4nja@gmail.com>
2024-01-30 16:07:04 -0800

    update file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,18 +2,8 @@
 
 const fs = require('fs');
 
-if (process.argv.length !== 4) {
-  console.error('Usage: ./1-writeme.js <file path> "<string to write>"');
-  process.exit(1);
-}
-
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-fs.writeFile(filePath, content, 'utf-8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
   if (err) {
-    console.error(err);
-  } else {
-    console.log(`Content has been written to ${filePath}`);
+    console.log(err);
   }
 });
